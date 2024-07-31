2ded6a796a3c76a430cf53b366109536827d97c7
iosdevyousra <eng1.yousra1@gmail.com>
2024-07-31 16:05:50 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by M-Store on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
